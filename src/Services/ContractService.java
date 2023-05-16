60c340398dbaa5e4e46bf75711479e84f59af32f
willgdj <120098718+willgdj@users.noreply.github.com>
2023-05-16 08:07:08 -0300

    class Contract finish class ContractService finish Interface OnlinePaymentService finish Program main OK

---
+++ src/Services/ContractService.java
@@ -0,0 +1,19 @@
+package Services;
+
+import entities.Contract;
+import entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months){
+        contract.getInstallments().add(new Installment(LocalDate.of(2018,7,25),206.04));
+        contract.getInstallments().add(new Installment(LocalDate.of(2018,8,25),208.08));
+    }
+}
--- src/entities/Installment.java
+++ src/entities/Installment.java
@@ -1,8 +1,11 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Installment {
+
+    private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private LocalDate date;
     private Double amount;
 
@@ -28,5 +31,10 @@ public class Installment {
 
     public void setAmount(Double amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date.format(fmt) + "-" + String.format("%.2f", amount);
     }
 }
